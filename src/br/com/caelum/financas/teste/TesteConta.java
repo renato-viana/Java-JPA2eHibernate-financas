fff07ccba3cdcc509e31f2f6fc89da20e90f8b86
Renato Viana <renatoviana30@gmail.com>
2020-04-04 10:24:45 -0300

    Projeto Java financas com JPA2 e Hibernate

---
+++ src/br/com/caelum/financas/teste/TesteConta.java
@@ -0,0 +1,33 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+
+		Conta conta = new Conta();
+		conta.setTitular("Danilo");
+		conta.setAgencia("123");
+		conta.setBanco("Banco do Brasil");
+		conta.setNumero("456");
+
+		EntityManager em = new JPAUtil().getEntityManager();
+
+		em.getTransaction().begin();
+		
+		conta = em.find(Conta.class, 1);
+		
+		em.remove(conta);
+		
+		conta.setBanco("Bradesco");
+		
+		em.getTransaction().commit();
+
+		em.close();
+
+	}
+}
