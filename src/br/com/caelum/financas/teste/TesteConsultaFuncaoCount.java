cd581fb7336c0cfafff3e0db1a02bab540700569
Renato Viana <renatoviana30@gmail.com>
2020-04-05 12:40:38 -0300

    Um pouco mais sobre queries e JPQL - TypedQuery

---
+++ src/br/com/caelum/financas/teste/TesteConsultaFuncaoCount.java
@@ -0,0 +1,25 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteConsultaFuncaoCount {
+
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+	    Conta conta = em.find(Conta.class, 2);//id 2 deve existir no banco
+	    
+	    Query query = em.createQuery("select count(m) from Movimentacao m where m.conta = :pConta");
+	    
+	    query.setParameter("pConta", conta);
+	    
+	    Long quantidade = (Long) query.getSingleResult();
+	    
+	    System.out.println("Total de movimentações: " + quantidade);
+	}
+
+}
